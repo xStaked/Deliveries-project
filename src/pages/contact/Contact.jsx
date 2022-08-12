b7cf24d7ffbfb1e6315a4b06c167f1187c3da722
= <=>
2022-08-11 22:28:43 -0500

    Added contact section destop and mobile view done

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,13 +1,118 @@
-import React from 'react'
-import {Container, Row, Col} from "react-bootstrap"
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import addressIcon from "../../assets/contactICONS/Address.png";
+import img1 from "../../assets/contactICONS/img1.png";
+import img2 from "../../assets/contactICONS/img2.png";
+import { BsFacebook } from "react-icons/bs";
+import { FiInstagram } from "react-icons/fi";
+import { BsLinkedin } from "react-icons/bs";
+import "./contact.Styles.scss";
 const Contact = () => {
   return (
-    <Container>
+    <Container className="contact-container">
+      <Row className="text-left m-3">
+        <h2 className="contact-title">Contacto</h2>
+      </Row>
+      <Row className="d-flex justify-content-around addres-img">
+        <Col className="d-flex flex-row justify-content-center align-items-center">
+          <img src={addressIcon} alt="addressIcon" className="mx-4" />
+          <div className="d-flex flex-column">
+            <span className="location">Cali - Sede Sur</span>
+            <span className="tel"> Tel: (602) 485 82 88 </span>
+            <span className="horarios-1">
+              {" "}
+              <span className="horarios">Horarios</span> L-V: 8:00-12:45pm y
+              2-4:45pm
+            </span>
+            <span className="horarios-2"> S: 8:00-12:45pm</span>
+          </div>
+        </Col>
+        <Col>
+          <img
+            src={img1}
+            alt="img1"
+            width={585}
+            height={495}
+            className="img1"
+          />
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-around align-items-center">
+        <Col className="m-2 d-flex justify-content-center align-items-center">
+          <img src={addressIcon} alt="addressIcon" />
+          <div className="d-flex flex-column">
+            <span className="location">Cali- Sede centro</span>
+            <span className="tel"> Tel: (602) 485 82 88 </span>
+            <span className="horarios-1">
+              <span className="horarios">Horarios</span> L-V: 8:00-12pm y
+              1:30-5:00pm
+            </span>
+            <span className="horarios-2">S: 8:00-12:00pm</span>
+          </div>
+        </Col>
+        <Col className="m-2 d-flex justify-content-center align-items-center">
+          <img src={addressIcon} alt="addressIcon" />
+          <div className="d-flex flex-column">
+            <span className="location">Barranquilla, Atlantico</span>
+            <span className="tel"> Tel: (605) 385 20 99 </span>
+            <span className="horarios-1">
+              <span className="horarios">Horarios</span> L-V: 8-12:30pm y
+              1:30-5:15pm
+            </span>
+            <span className="horarios-2"> S: 8:00-12:00pm</span>
+          </div>
+        </Col>
+      </Row>
       <Row>
-        <h2>Contacto</h2>
+        <Col className="m-5 d-flex justify-content-center align-items-center">
+          <img src={addressIcon} alt="addressIcon" />
+          <div className="d-flex flex-column">
+            <span className="location">Pereira, Risaralda </span>
+            <span className="tel"> Cel: (+57) 316 453 5301 </span>
+            <span className="horarios-1">
+              <span className="horarios">Horarios</span> L-V: 8-12:30pm y
+              1:30-5:00pm
+            </span>
+            <span className="horarios-2"> S: 8:00-12:00pm</span>
+          </div>
+        </Col>
+        <Col className="m-5 d-flex justify-content-center align-items-center">
+          <img src={addressIcon} alt="addressIcon" />
+          <div className="d-flex flex-column">
+            <span className="location">Bogotá, Cundinamarca </span>
+            <span className="tel"> Tel: (601) 745 73 12 </span>
+            <span className="horarios-1">
+              {" "}
+              <span className="horarios">Horarios</span> L-V: 7:30-12:30pm y
+              1:30-5pm
+            </span>
+            <span className="horarios-2"> S: 8:00-12:00pm</span>
+          </div>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="d-flex justify-content-center align-items-center">
+          <img src={img2} alt="img2" width={500} height={500}  className="img2"/>
+        </Col>
+
+        <Col >
+            <h3 className="redes-title">Redes sociales</h3>
+          <div className="d-flex flex-row justify-content-center align-items-center">
+            <a>
+              <BsFacebook className="icon" />
+            </a>
+            <a>
+              <FiInstagram className="icon" />
+            </a>
+            <a>
+              <BsLinkedin className="icon" />
+            </a>
+          </div>
+        </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import signUp from "../../assets/signUp.png";
 import { Link } from "react-router-dom";
@@ -31,6 +31,57 @@ const Register = () => {
 
    }
 
+   const RenderRegister = () => {
+    return (
+      <Col className="d-flex flex-column justify-content-center align-items-center">
+        <h3 className="title-login">Register</h3>
+        <form
+          onSubmit={handleSubmit}
+          className="d-flex flex-column justify-content-center align-items-center"
+        >
+          <input
+            type="text"
+            placeholder="Correo electronico"
+            className="form-control mb-3"
+            name="email_user"
+            required
+            onChange={handleChangeEmail}
+          />
+  
+          <input
+            type="password"
+            placeholder="Contraseña"
+            className="form-control mb-3"
+            required
+            name="password_user"
+            onChange={handleChangePassword}
+          />
+  
+          <input
+            type="password"
+            placeholder="Confirmar Contraseña"
+            className="form-control mb-3"
+            required
+            name="password_user"
+            onChange={handleChangePasswordConfirm}
+          />
+  
+          <input
+            type="submit"
+            value="Iniciar sesión"
+            className="form-control mb-3 login-button"
+          />
+        </form>
+        <div className="d-flex flex-row justify-content-center text-contain">
+          <p className="mx-3 qa">¿No tienes cuenta? </p>
+          <Link to="register" className="text-center text-decoration-none">
+            <span className="register-log">Register</span>
+          </Link>
+        </div>
+      </Col>
+    );
+  };
+
 
   return (
     <div className="container-register">
@@ -56,53 +107,4 @@ const Register = () => {
 
 export default Register;
 
-const RenderRegister = () => {
-  return (
-    <Col className="d-flex flex-column justify-content-center align-items-center">
-      <h3 className="title-login">Register</h3>
-      <form
-        onSubmit={handleSubmit}
-        className="d-flex flex-column justify-content-center align-items-center"
-      >
-        <input
-          type="text"
-          placeholder="Correo electronico"
-          className="form-control mb-3"
-          name="email_user"
-          required
-          onChange={handleChangeEmail}
-        />
 
-        <input
-          type="password"
-          placeholder="Contraseña"
-          className="form-control mb-3"
-          required
-          name="password_user"
-          onChange={handleChangePassword}
-        />
-
-        <input
-          type="password"
-          placeholder="Confirmar Contraseña"
-          className="form-control mb-3"
-          required
-          name="password_user"
-          onChange={handleChangePasswordConfirm}
-        />
-
-        <input
-          type="submit"
-          value="Iniciar sesión"
-          className="form-control mb-3 login-button"
-        />
-      </form>
-      <div className="d-flex flex-row justify-content-center text-contain">
-        <p className="mx-3 qa">¿No tienes cuenta? </p>
-        <Link to="register" className="text-center text-decoration-none">
-          <span className="register-log">Register</span>
-        </Link>
-      </div>
-    </Col>
-  );
-};
